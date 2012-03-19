e36a784eddae382086c004c853508e3d00256b25
dhacker29 <davidhackerdvm@gmail.com>
2012-03-19 12:29:34 -0500

    Update to use Vendor AOKP

--- device_targa.mk
+++ device_targa.mk
@@ -111,8 +111,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     Camera \
-    Superuser \
-    su \
     Usb \
     DockAudio \
 
@@ -120,8 +118,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory \
-    FileManager \
-    MusicFX \
     libjni_pinyinime \
 
 # CameraFix
@@ -212,7 +208,6 @@ PRODUCT_COPY_FILES += \
 # Phone settings
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    vendor/aokp/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip \
 
 #    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml \
 
@@ -259,7 +254,6 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product, hardware/ti/camera/camera.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
-#$(call inherit-product-if-exists, vendor/google/google-vendor.mk)
 $(call inherit-product-if-exists, vendor/verizon/targa-verizon-vendor.mk)
 
 $(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor.mk)
